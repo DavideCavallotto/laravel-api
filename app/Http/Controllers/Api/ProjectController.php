c8d6f73b284699751e00ef971eefb5cff13bef8c
DavideCavallotto <davidecavallotto92@gmail.com>
2024-01-16 17:19:36 +0100

    ADD route show and function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,13 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
+
+    public function show(Project $project) {
+
+        $project->load('technologies', 'type');
+
+        return response()->json([
+            'project' => $project
+        ]);
+    }
 }
